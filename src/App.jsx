57809ba68b6952498ad595843986489cc1bea40b
Tarunksaini <tarunshr145@gmail.com>
2024-08-15 14:33:14 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="box">
+        <h1>Let's build TINDER clone 🚀</h1>
+
+        {/* Header */}
+        {/* Tinder-card */}
+        {/* button container */}
+
+        {/* Chat screen  */}
+      </div>
+    </>
+  )
+}
+
+export default App
